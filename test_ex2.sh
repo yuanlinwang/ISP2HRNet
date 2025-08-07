30c3eb27c51958e6d3e12d061b58d6f1fddaca5b
Yuanlin Wang <wangyuanlin0121@foxmail.com>
2025-08-07 10:10:24 +0800

    Update test_ex2.sh

--- test_ex2.sh
+++ test_ex2.sh
@@ -1,86 +1,86 @@
-# echo 'set5' &&
-# echo '0.5' &&
-# echo 'x2' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-set5-2-rrs+sr.yaml --dataset_name 'set5'&&
-# echo 'x3' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-set5-3-rrs+sr.yaml --dataset_name 'set5'&&
-# echo 'x4' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 4  --config configs/test-irregular/test-set5-4-rrs+sr.yaml --dataset_name 'set5'&&
-# echo 'x1.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-set5-1.5-rrs+sr.yaml --dataset_name 'set5'&&
-# echo 'x2.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-set5-2.5-rrs+sr.yaml --dataset_name 'set5'&&
-# echo 'x6' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-set5-6-rrs+sr.yaml --dataset_name 'set5'&&
-# echo 'x8' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-set5-8-rrs+sr.yaml --dataset_name 'set5'&&
+echo 'set5' &&
+echo '0.5' &&
+echo 'x2' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-set5-2-rrs+sr.yaml --dataset_name 'set5'&&
+echo 'x3' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-set5-3-rrs+sr.yaml --dataset_name 'set5'&&
+echo 'x4' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 4  --config configs/test-irregular/test-set5-4-rrs+sr.yaml --dataset_name 'set5'&&
+echo 'x1.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-set5-1.5-rrs+sr.yaml --dataset_name 'set5'&&
+echo 'x2.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-set5-2.5-rrs+sr.yaml --dataset_name 'set5'&&
+echo 'x6' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-set5-6-rrs+sr.yaml --dataset_name 'set5'&&
+echo 'x8' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-set5-8-rrs+sr.yaml --dataset_name 'set5'&&
 
-# echo 'set14' &&
-# echo '0.5' &&
-# echo 'x2' &&
+echo 'set14' &&
+echo '0.5' &&
+echo 'x2' &&
 python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-set14-2-rrs+sr.yaml --dataset_name 'set14'&&
-# echo 'x3' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-set14-3-rrs+sr.yaml --dataset_name 'set14'&&
-# echo 'x4' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-set14-4-rrs+sr.yaml --dataset_name 'set14'&&
-# echo 'x1.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-set14-1.5-rrs+sr.yaml --dataset_name 'set14'&&
-# echo 'x2.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-set14-2.5-rrs+sr.yaml --dataset_name 'set14'&&
-# echo 'x6' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-set14-6-rrs+sr.yaml --dataset_name 'set14'&&
-# echo 'x8' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-set14-8-rrs+sr.yaml --dataset_name 'set14'&&
+echo 'x3' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-set14-3-rrs+sr.yaml --dataset_name 'set14'&&
+echo 'x4' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-set14-4-rrs+sr.yaml --dataset_name 'set14'&&
+echo 'x1.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-set14-1.5-rrs+sr.yaml --dataset_name 'set14'&&
+echo 'x2.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-set14-2.5-rrs+sr.yaml --dataset_name 'set14'&&
+echo 'x6' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-set14-6-rrs+sr.yaml --dataset_name 'set14'&&
+echo 'x8' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-set14-8-rrs+sr.yaml --dataset_name 'set14'&&
 
-# echo 'bsd100' &&
-# echo '0.5' &&
-# echo 'x2' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-bsd100-2-rrs+sr.yaml --dataset_name 'bsd100'&&
-# echo 'x3' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-bsd100-3-rrs+sr.yaml --dataset_name 'bsd100'&&
-# echo 'x4' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-bsd100-4-rrs+sr.yaml --dataset_name 'bsd100'&&
-# echo 'x1.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-bsd100-1.5-rrs+sr.yaml --dataset_name 'bsd100'&&
-# echo 'x2.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-bsd100-2.5-rrs+sr.yaml --dataset_name 'bsd100'&&
-# echo 'x6' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-bsd100-6-rrs+sr.yaml --dataset_name 'bsd100'&&
-# echo 'x8' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-bsd100-8-rrs+sr.yaml --dataset_name 'bsd100'&&
+echo 'bsd100' &&
+echo '0.5' &&
+echo 'x2' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-bsd100-2-rrs+sr.yaml --dataset_name 'bsd100'&&
+echo 'x3' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-bsd100-3-rrs+sr.yaml --dataset_name 'bsd100'&&
+echo 'x4' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-bsd100-4-rrs+sr.yaml --dataset_name 'bsd100'&&
+echo 'x1.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-bsd100-1.5-rrs+sr.yaml --dataset_name 'bsd100'&&
+echo 'x2.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-bsd100-2.5-rrs+sr.yaml --dataset_name 'bsd100'&&
+echo 'x6' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-bsd100-6-rrs+sr.yaml --dataset_name 'bsd100'&&
+echo 'x8' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-bsd100-8-rrs+sr.yaml --dataset_name 'bsd100'&&
 
-# echo 'urban100' &&
-# echo '0.5' &&
-# echo 'x2' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-urban100-2-rrs+sr.yaml --dataset_name 'urban100'&&
-# echo 'x3' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-urban100-3-rrs+sr.yaml --dataset_name 'urban100'&&
-# echo 'x4' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-urban100-4-rrs+sr.yaml --dataset_name 'urban100'&&
-# echo 'x1.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-urban100-1.5-rrs+sr.yaml --dataset_name 'urban100'&&
-# echo 'x2.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-urban100-2.5-rrs+sr.yaml --dataset_name 'urban100'&&
-# echo 'x6' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-urban100-6-rrs+sr.yaml --dataset_name 'urban100'&&
-# echo 'x8' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-urban100-8-rrs+sr.yaml --dataset_name 'urban100'&&
+echo 'urban100' &&
+echo '0.5' &&
+echo 'x2' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-urban100-2-rrs+sr.yaml --dataset_name 'urban100'&&
+echo 'x3' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-urban100-3-rrs+sr.yaml --dataset_name 'urban100'&&
+echo 'x4' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-urban100-4-rrs+sr.yaml --dataset_name 'urban100'&&
+echo 'x1.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-urban100-1.5-rrs+sr.yaml --dataset_name 'urban100'&&
+echo 'x2.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-urban100-2.5-rrs+sr.yaml --dataset_name 'urban100'&&
+echo 'x6' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-urban100-6-rrs+sr.yaml --dataset_name 'urban100'&&
+echo 'x8' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-urban100-8-rrs+sr.yaml --dataset_name 'urban100'&&
 
-# echo 'div2k' &&
-# echo '0.5' &&
-# echo 'x2' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-div2k-2-rrs+sr.yaml --dataset_name 'div2k'&&
-# echo 'x3' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-div2k-3-rrs+sr.yaml --dataset_name 'div2k'&&
-# echo 'x4' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-div2k-4-rrs+sr.yaml --dataset_name 'div2k'&&
-# echo 'x1.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-div2k-1.5-rrs+sr.yaml --dataset_name 'div2k'&&
-# echo 'x2.5' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-div2k-2.5-rrs+sr.yaml --dataset_name 'div2k'&&
-# echo 'x6' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-div2k-6-rrs+sr.yaml --dataset_name 'div2k'&&
-# echo 'x8' &&
-# python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-div2k-8-rrs+sr.yaml --dataset_name 'div2k'&&
+echo 'div2k' &&
+echo '0.5' &&
+echo 'x2' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2 --config configs/test-irregular/test-div2k-2-rrs+sr.yaml --dataset_name 'div2k'&&
+echo 'x3' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 3 --config configs/test-irregular/test-div2k-3-rrs+sr.yaml --dataset_name 'div2k'&&
+echo 'x4' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 4 --config configs/test-irregular/test-div2k-4-rrs+sr.yaml --dataset_name 'div2k'&&
+echo 'x1.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 1.5 --config configs/test-irregular/test-div2k-1.5-rrs+sr.yaml --dataset_name 'div2k'&&
+echo 'x2.5' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 2.5 --config configs/test-irregular/test-div2k-2.5-rrs+sr.yaml --dataset_name 'div2k'&&
+echo 'x6' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 6 --config configs/test-irregular/test-div2k-6-rrs+sr.yaml --dataset_name 'div2k'&&
+echo 'x8' &&
+python test-irregular_ex2.py   --ratio 0.5   --scale 8 --config configs/test-irregular/test-div2k-8-rrs+sr.yaml --dataset_name 'div2k'&&
 
 true
